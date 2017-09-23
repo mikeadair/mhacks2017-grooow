d09dcfda4bac2f463d470f35b035e8dcd33f9032
kylezimmer <zimmerk4@msu.edu>
2017-09-23 08:37:23 -0400

    Added data

--- Parsers/DemosParsers.py
+++ Parsers/DemosParsers.py
@@ -1,1 +1,11 @@
-# Done already
+import csv
+
+
+with open("../Data Sets/DirtyDataSets/Demolitions.csv") as demos, \
+    open("../Data Sets/ParsedData/demolitions.csv", "w") as ndemos:
+    demosreader = csv.reader(demos)
+    next(demosreader)
+    ndemos.write("date,district,latitude,longitude\n")
+    for line in demosreader:
+        ndemos.write(','.join([line[4], line[6], line[7], line[8]]) + '\n')
+
